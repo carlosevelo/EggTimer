4d7bb614239e634407692f7def09e7ac6c7da56a
Carlos Evelo <carlosevelo96@gmail.com>
2023-02-08 10:24:22 -0700

    timer

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let timeDictionary = ["Soft": 5, "Medium": 7, "Hard": 12]
+    var timer = Timer()
+
+    @IBOutlet weak var timeLabel: UILabel!
     
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle!
+        var seconds = timeDictionary[hardness]!
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self] _ in
+            if seconds > 0 {
+                print(seconds)
+                seconds -= 1
+            }
+            else {
+                timer.invalidate()
+                timeLabel.text = "Done!"
+            }
+        }
 
-
+    }
+    
 }
