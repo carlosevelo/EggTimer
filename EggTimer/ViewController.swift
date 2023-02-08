15d903b1ad57cfb9d857e225fb1a871427c92798
Carlos Evelo <carlosevelo96@gmail.com>
2023-02-08 12:10:14 -0700

    progress bar

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,28 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let timeDictionary = ["Soft": 5, "Medium": 7, "Hard": 12]
+    let timeDictionary = ["Soft": 5.0, "Medium": 7.0, "Hard": 12.0]
     var timer = Timer()
 
     @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         timer.invalidate()
+        progressBar.progress = 0.0
         
         let hardness = sender.currentTitle!
-        var seconds = timeDictionary[hardness]!
+        let seconds = timeDictionary[hardness]!
+        timeLabel.text = hardness
         
+        startTimer(totalSeconds: seconds)
+    }
+
+    func startTimer(totalSeconds: Double) {
+        var secondsPassed = 0.0
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self] _ in
-            if seconds > 0 {
-                print(seconds)
-                seconds -= 1
+            if secondsPassed < totalSeconds {
+                secondsPassed += 1
+                progressBar.progress = Float(secondsPassed/totalSeconds)
             }
             else {
                 timer.invalidate()
                 timeLabel.text = "Done!"
+                progressBar.progress = 1.0
             }
         }
-
     }
     
 }
